50c0eaf4619d1eb94afce77c5c9b33f9efe849c0
Ernani <ernanidapaz422@gmail.com>
2023-09-04 20:30:09 -0300

    atualizacao

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE <html>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+    <h1> sistema Mentor </h1>
+    <div>
+        <h2> Faça seu login:</h2>
+        <form action= "#" method="POST">
+            <input type="text">
+            <input type="text">
+        </form>
+    </div> 
+    
+    <div>
+        <h2> Faça seu cadastro </h2>
+        <form action= "#" method="POST">
+            <label for="cadastro-login">Login: </label>
+            <input type="text">
+
+            <label for="cadastro-email">Email; </label>
+            <input type="password">
+
+            <label for="cadastro-senha">senha: </label>
+            <input type="password">
+
+            <label for="cadastro-confirmcao-senha">cadastro-confirmcao-senha </label>
+            <input type="password">
+
+</body>
+</html>
+</html>
