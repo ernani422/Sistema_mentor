ea9d034f39c7d60d0de7f8a1321f9ad1bc7cf215
Ernani <ernanidapaz422@gmail.com>
2023-10-02 20:40:20 -0300

    criacao de tabela

---
+++ database/migrations/2023_10_02_222920_create_aulas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aulas', function (Blueprint $table) {
+            $table->id();
+            $table->date('data');
+            $table->time('horaInicio');
+            $table->time('horaTermino');
+
+            $table->bigInteger('disciplina_id')->unsigned();
+            $table->foreign('disciplina_id')->references('id')->on('disciplinas');
+
+            $table->bigInteger('turma_id')->unsigned();
+            $table->foreign('turma_id')->references('id')->on('turmas');
+
+            $table->bigInteger('professor_id')->unsigned();
+            $table->foreign('professor_id')->references('id')->on('professores');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aulas');
+    }
+};
---
+++ database/migrations/2023_10_02_224040_create_turma_diciplinas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('turma_diciplinas', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('turma_id')->unsigned();
+            $table->foreign('turma_id')->references('id')->on('turmas');
+
+            $table->bigInteger('disciplina_id')->unsigned();
+            $table->foreign('disciplina_id')->references('id')->on('disciplinas');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('turma_diciplinas');
+    }
+};
